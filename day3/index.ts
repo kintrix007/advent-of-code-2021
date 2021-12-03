22e64ec475be8beadd8b89b4d39e3eb3a847ad85
kintrix <boldizsar02@gmail.com>
2021-12-03 17:48:57 +0100

    Solved day 3 part 1

---
+++ day3/index.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+
+function main(): void {
+    const cont = fs.readFileSync("input").toString().trim().split("\n");
+    const len = cont[0]!.length;
+
+    const tmpCommonBits = cont.map(x => x.split("").map( x => +x * 2 - 1 ))
+                              .reduce((acc, x) => acc.map( (_, i) => acc[i]! + x[i]! ))
+                              .map(x => +(x > 0))
+                              .join("");
+    const commonBits = parseInt(tmpCommonBits, 2);
+    const max = (1 << len) - 1;
+    console.log(commonBits * (max - commonBits))
+    
+}
+
+main();
